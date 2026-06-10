23c8b1ffe462b5a63b33fdfce79652f6fe8390be
Guillermo Iván Obregón Saucedo <A01385883@tec.mx>
2026-06-09 18:38:30 -0600

    BD juego 7

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     public bool debugMode = false;
     private bool gameOver = false;
 
-    [SerializeField] int[] NivelesAumentosDificultad = { 6, 11, 17, 25 };
+    [SerializeField] int[] NivelesAumentosDificultad = { 7, 13, 20, 28 };
     [SerializeField] float[] TiempoAumentosDificultad = { 1.3f, 1.6f, 2.0f, 2.5f };
     private string[] minijuegosnombres = {"CriticAlerts","Memorama","RegisteredComponents" , "Masher", "Botones", "PurdueModel" ,"CatchAnomalies"};
 
@@ -275,6 +275,13 @@ public class GameManager : MonoBehaviour
                 if (difficulty == 3) return 28;
                 if (difficulty == 4) return 29;
                 return 30;
+
+            case "CatchAnomalies":
+                if (difficulty == 1) return 31;
+                if (difficulty == 2) return 32;
+                if (difficulty == 3) return 33;
+                if (difficulty == 4) return 34;
+                return 35;
 
             default:
                 return -1; // No es un minijuego (ej: Intermission, PantallaInicial, Puntuaje)
